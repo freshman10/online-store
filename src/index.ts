326121be68f4ff71d5a1f8dfe23730fb2a8b780c
Alexander Marunich <myrakes4korn@gmail.com>
2022-07-16 10:57:41 +0300

    feat: add self-check

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { buildPage } from './components/buildPage';
 import '../src/global.css';
 import { runController } from './components/controller/runController';
+import { selfCheck } from './constants/constants';
 
 buildPage();
 runController();
+console.log(selfCheck);
