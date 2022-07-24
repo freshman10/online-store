0c23f361ae6d97128464efcbae378f6d1e8e404b
Alexander Marunich <myrakes4korn@gmail.com>
2022-07-24 13:01:56 +0300

    refactor: remove dead code

--- src/components/filters/search.ts
+++ src/components/filters/search.ts
@@ -1,18 +1,5 @@
-import { elementDomStorage } from '../render/generateElement';
-import { renderItems } from '../render/items';
 import { DataObject } from '../../constants/types';
-import { applyAll } from './applyAllFilters';
-import { INPUT, INPUT_SEARCH } from '../../constants/constants';
 
 export function search(data: DataObject[], pattern: string): DataObject[] {
     return data.filter((el) => el.name.toLocaleLowerCase().includes(pattern.toLocaleLowerCase()));
-}
-
-export function addSearch(data: DataObject[]): void {
-    elementDomStorage.get(INPUT_SEARCH)?.forEach((el) => {
-        el.addEventListener(INPUT, () => {
-            const filtered = applyAll(data);
-            renderItems(filtered);
-        });
-    });
 }
